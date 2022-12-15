b097f41562bf3a4b8bf5a86188ae08d82e3a8a79
mattiamonti <mattiamonti2001@gmail.com>
2022-12-15 22:34:44 +0100

    MacBookPro

---
+++ cover-reveal.js
@@ -0,0 +1,4 @@
+gsap.to("#cover", {
+    x: -3000, opacity: 1, duration: 2, ease: 'in', delay: 0.3
+});
+
---
+++ script.js
@@ -0,0 +1,5 @@
+gsap.to("#arrow-tip1", {
+    x: '50%', delay: 3, duration: 1, yoyo: true, repeat: -1, repeatDelay: 0.2, ease: "power1.inOut"
+});
+
+

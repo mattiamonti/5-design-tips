394f1e64b969afa7bf6f0c895214fc0d9b879c0a
mattiamonti <mattiamonti2001@gmail.com>
2022-12-16 13:51:04 +0100

    aggiunte animazioni, strutturato codice e pulito

---
+++ js/animation-tip2.js
@@ -0,0 +1,3 @@
+gsap.from(".quality-image", {
+    delay: 1, duration: 1.5, opacity: 0, ease: "power1.Out"
+});
---
+++ js/animation-tip3.js
@@ -0,0 +1,3 @@
+gsap.from("#mobile", {
+    delay: 1, duration: 1, y: 1000, ease: "power1.Out"
+});
